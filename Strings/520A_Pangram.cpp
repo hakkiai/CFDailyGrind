56a48aa8cfef50b27c946fbf952ca1e90a6248f0
hakkiai <abhishektripathikuberji@gmail.com>
2025-01-30 21:47:59 +0530

    Implement solution for problem 520A - Pangram and for 1678B - Equal Candles

--- Strings/520A_Pangram.cpp
+++ Strings/520A_Pangram.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main(){
-    // need to add the solution here....
+    
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0;i<n;i++){
+        if(s[i]>='A' && s[i]<='Z'){
+            s[i] = s[i] + 32;
+        }
+    }
+    int arr[26] = {0};
+    for(int i=0;i<n;i++){
+        arr[s[i]-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        if(arr[i]==0){
+            cout<<"NO";
+            return 0;
+        }
+    }
+    cout<<"YES";
+    
+
+    return 0;
 }
