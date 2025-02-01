9060b256590ee5a3d01cdea0d969117a204d5088
hakkiai <abhishektripathikuberji@gmail.com>
2025-02-01 23:22:29 +0530

    Implement solution for problem 1678B - Equal Candles

--- Sorting/1678B_EqualCandles.cpp
+++ Sorting/1678B_EqualCandles.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(){
-    
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for(int i=0; i<n;i++){
+            cin >> a[i];
+        }
+
+        int min = *min_element(a.begin(), a.end());
+
+        for(int i=0; i<n; i++){
+            a[i] -= min;
+
+        }
+
+       long long sum = 0;
+        for(int i=0; i<n; i++){
+            sum += (a[i] - min);
+        }
+
+        cout << sum << endl;
+
+    }
 }
