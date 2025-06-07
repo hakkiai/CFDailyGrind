41b1b05fee5324d313c0b965b25f68d4c159fe2f
hakkiai <abhishektripathikuberji@gmail.com>
2025-06-07 12:28:22 +0530

    Added Solution for 282A Bit++

--- Implementation/282A_Bit_plus_plus.cpp
+++ Implementation/282A_Bit_plus_plus.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin >> n;
+    int x = 0;
+    for(int i=0; i<n;i++){
+        string s;
+        cin >> s;
+        if(s.find("++") != string::npos){
+            x++;
+        }else{
+            x--;
+        }
+    }
+
+    cout << x;
+
+    return 0;
 }
